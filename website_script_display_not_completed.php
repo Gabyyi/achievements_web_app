5c9bc12b08682d152915d777650db79208e55995
Gabyyi <90030416+Gabyyi@users.noreply.github.com>
2024-01-27 14:10:23 +0200

    Update website_script_display_not_completed.php

--- website_script_display_not_completed.php
+++ website_script_display_not_completed.php
@@ -1,8 +1,8 @@
 <?php
 $servername="localhost";
-$username="gabi";
-$password="12345";
-$databasename="website_database";
+$username="user";
+$password="password";
+$databasename="database";
 $database_connection=mysqli_connect($servername , $username , $password , $databasename);
 
 if (!$database_connection) {
@@ -17,7 +17,7 @@ echo "Successfully connected to database: $databasename";
 	<body>
 	<br><br>This is the PHP script that display the database<br><br>
 <?php	
-	$database_query="SELECT * FROM website_database.achievementsid WHERE status LIKE 'not_completed' ORDER BY completion_date ASC";
+	$database_query="SELECT * FROM database.achievements WHERE status LIKE 'not_completed' ORDER BY completion_date ASC";
 	mysqli_query($database_connection, $database_query) or die("Query error to database: $databasename");
 
 	$query_result=mysqli_query($database_connection, $database_query);
