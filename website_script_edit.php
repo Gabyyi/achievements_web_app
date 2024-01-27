0b13bad5ede8517d6b79da56fcd1395bed4a5b1d
Gabyyi <90030416+Gabyyi@users.noreply.github.com>
2024-01-27 14:11:16 +0200

    Update website_script_edit.php

--- website_script_edit.php
+++ website_script_edit.php
@@ -1,8 +1,8 @@
 <?php
 $servername="localhost";
-$username="gabi";
-$password="12345";
-$databasename="website_database";
+$username="user";
+$password="password";
+$databasename="database";
 $database_connection=mysqli_connect($servername , $username , $password , $databasename);
 
 if (!$database_connection) {
@@ -17,10 +17,10 @@ echo "Successfully connected to database: $databasename";
 	<body>
 	<br><br>This is the PHP script that edit data and then reads the database<br><br>
 <?php
-	$database_query="UPDATE website_database.achievementsid SET completion_date='$_POST[date]' , status='$_POST[status]' WHERE achievements='$_POST[achievement]'";
+	$database_query="UPDATE database.achievements SET completion_date='$_POST[date]' , status='$_POST[status]' WHERE achievements='$_POST[achievement]'";
 	mysqli_query($database_connection, $database_query) or die("Query error to database: $databasename");
 
-	$database_query="SELECT * FROM website_database.achievementsid";
+	$database_query="SELECT * FROM database.achievements";
 	mysqli_query($database_connection, $database_query) or die("Query error to database: $databasename");
 
 	$query_result=mysqli_query($database_connection, $database_query);
